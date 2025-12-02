42d3158b0ae21547fcada2d843cbd00491239970
onody <onoderayuto@onody00.local>
2025-12-02 23:33:04 +0900

    swagger

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('App') // ← Swagger のグループ名
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Hello を返します' })
+  @ApiResponse({ status: 200, description: '成功', type: String })
   getHello(): string {
     return this.appService.getHello();
   }
