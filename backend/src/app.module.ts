91fc1b1b11f920e887e50ff2fb3602570a3ad69f
onody <onoderayuto@onody00.local>
2025-12-02 23:49:02 +0900

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,       // docker-compose.yml の DB_HOST=db と一致
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,          // entity を自動読み込み
+      synchronize: true,               // 開発中のみ true（本番は false）
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
